ba973935092af5b3fd4af7c4c6792c0bc55c8810
lucas.caminha <lucas_santoscaminha@hotmail.com>
2021-11-26 19:48:07 -0300

    commit inicial do projeto

---
+++ app/src/main/java/br/com/ucsal/mobile/moedas/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.ucsal.mobile.moedas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
