6a36d42744d2481e60f15183ad2acefdb1694607
lucas.caminha <lucas_santoscaminha@hotmail.com>
2021-11-28 19:57:04 -0300

    Atualização de cores da variação da moeda positiva e negativa.

--- app/src/main/java/br/com/ucsal/mobile/moedas/adapter/MoedasListAdapter.java
+++ app/src/main/java/br/com/ucsal/mobile/moedas/adapter/MoedasListAdapter.java
@@ -1,11 +1,13 @@
 package br.com.ucsal.mobile.moedas.adapter;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
 import java.text.Format;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -47,25 +49,29 @@ public class MoedasListAdapter extends BaseAdapter {
         View v = activity.getLayoutInflater().inflate(R.layout.lista_moeda, viewGroup, false);
         Moeda moeda = moedas.get(i);
 
-        TextView codigo = v.findViewById(R.id.text1);
-        TextView nome = v.findViewById(R.id.text2);
-        TextView high = v.findViewById(R.id.text3);
-        TextView low = v.findViewById(R.id.text4);
-        TextView var = v.findViewById(R.id.text5);
+        TextView codigo = v.findViewById(R.id.cod_name);
+        TextView cotacao = v.findViewById(R.id.cotacao);
+        TextView var = v.findViewById(R.id.variacao);
         TextView dtUltimaAtualizacao = v.findViewById(R.id.data_ultima_atualizacao);
 
-        codigo.setText("Moeda: " + moeda.getCode() + " - " + moeda.getName());
-        high.setText("Cotação mais alta: " + moeda.getHigh());
-        low.setText("Cotação mais baixa : " + moeda.getLow());
-        var.setText("Variação: " + moeda.getVarBid());
-        dtUltimaAtualizacao.setText("Ultima atualização: " + moeda.getCreate_date());
+        DecimalFormat df = new DecimalFormat();
+        df.setMaximumFractionDigits(3);
 
+        codigo.setText("Moeda: " + moeda.getCode() + " - " + moeda.getName());
+
+        if (moedas.get(i).getVarBid() >= 0.00){
+            cotacao.setText("Cotação: " + df.format(moeda.getHigh()));
+            var.setText("Variação: +" + df.format(moeda.getVarBid()));
+            var.setTextColor(Color.parseColor("#008000"));
+        } else {
+            cotacao.setText("Cotação: " +  df.format(moeda.getLow()));
+            var.setText("Variação: " + df.format(moeda.getVarBid()));
+            var.setTextColor(Color.parseColor("#FF0000"));
+        }
+
+        dtUltimaAtualizacao.setText("Atualização: " + moeda.getCreate_date());
 
         return v;
-
-
-       // View v = activity.getLayoutInflater().inflate(R.layout.)
-       // return null;
     }
 
 }
